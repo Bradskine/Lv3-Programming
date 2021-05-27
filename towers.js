1503631d23d1bd26b7024a743a68ba698a9ce3d3
Bradskine <51344446+Bradskine@users.noreply.github.com>
2021-05-27 23:09:07 +1200

    Add files via upload
    setup basic tower class and also started planning 1st towers

---
+++ towers.js
@@ -0,0 +1,22 @@
+class tower { //this will creawte circles which go around track
+
+  constructor(
+  attackSpeed = 0,
+  pierce =  0,
+  range=0,
+  damage = 0
+
+
+
+
+  ) {
+    this.attackSpeed = attackSpeed;
+    this.pierce = pierce;
+    this.range = range;
+    this.damage = damage;
+  }
+
+
+
+
+}  //end of tower class
